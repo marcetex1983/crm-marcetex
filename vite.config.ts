4fa4877924ad3cd030a1be70ec920287bae614fb
Equipe Marcetex <desenvolvedor@marcetex.com.br>
2026-04-02 15:36:52 -0300

    alterações

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: Number(process.env.PORT) || 8080,
+    host: '0.0.0.0',
+    allowedHosts: true
+  }
 })
